d385813081814d81ecda4837967b21838b602c45
Safeerygr <safeeropuzmini@gmail.com>
2026-05-09 12:33:25 +0530

    Add greetings script based on current time

---
+++ greetings.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+h=`date +%H`
+
+currenttime=$h
+echo "$currenttime"
+if [ $h -lt 12 ]
+then
+        echo "Good morining"
+elif [ $h -lt 16 ]
+then
+        echo "good After noon"
+elif [ $h -lt 20 ]
+then
+        echo "good Evening"
+else
+        echo "good night"
+fi
+
+~
+~
