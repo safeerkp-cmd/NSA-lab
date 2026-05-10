a2385a018a11a6679f34bf328531fb20bdf24c9c
Safeerygr <safeeropuzmini@gmail.com>
2026-05-10 18:09:14 +0530

    Create count.sh for line and word counting
    Add a script to count lines and words in a file

---
+++ count.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+
+if [ $# -eq 0 ]
+then
+        echo "erorr:one file needed"
+        exit 1
+fi
+
+word=0
+line=0
+while read l
+do
+        line=` expr $line + 1 `
+        for w in $l
+        do
+                word=` expr $word + 1 `
+        done
+done <  $1
+echo "Number of line = $line"
+echo "numberof words = $word"
+~
